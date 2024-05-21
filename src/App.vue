8e234e9db206d016152238c0fd12257afdd026a4
Ramiro Gumma <ramirogumma@hotmail.com>
2024-05-20 21:01:26 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HeroComponent from './components/HeroComponent.vue'
+</script>
+
+<template>
+  <main>
+    <HeroComponent />
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Login</li>
+        <li>Signin</li>
+      </ul>
+    </nav>
+  </div>
+</template>
+<style scoped></style>
